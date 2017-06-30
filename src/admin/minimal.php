2e691d68b6ff7a61756b706f9ad6e4527277c742
application2000 <contact@application2000.com>
2017-06-30 18:23:49 -0400

    First commit

---
+++ src/admin/minimal.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package              com_dataful dataful.php
+ * @author               Alexandre E. <developpeur@application2000.com>
+ * @link                 https://www.application2000.com
+ * @copyright        (C) 2017 www.application2000.com . Tous droits réservés.
+ * @version              GIT: $Id$
+ **/
+defined('_JEXEC') or die;
+
+echo 'backend';
