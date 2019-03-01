5440608056ddbeece866914690cd43f8af158fe5
Alexandre ELISÉ <developpeur@application2000.com>
2019-03-01 15:01:34 -0400

    Update minimal.php

--- src/site/minimal.php
+++ src/site/minimal.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
  * @package              minimal.php
  * @author               Alexandre ELISÉ <developpeur@application2000.com>
  * @link                 https://www.application2000.com
- * @copyright        (C) 2017 www.application2000.com . Tous droits réservés.
+ * @copyright        (C) 2019 www.application2000.com . Tous droits réservés.
  * @version              GIT: $Id$
  **/
 defined('_JEXEC') or die;
